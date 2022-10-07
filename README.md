e97c6d5d33e8650091090f3bafcc8ac542499912
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-07 12:20:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-c17-l6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-7yym9a)
---
+++ src/pages/Profile/Profile.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+
+export default function Profile() {
+  return (
+    <>
+      <ul className="d-flex p-20">
+        <li>
+          <NavLink end to="">
+            Overview
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="data">Data</NavLink>
+        </li>
+      </ul>
+      <div className="p-20">
+        <h2 className="mb-20">Profile</h2>
+        <Outlet />
+      </div>
+    </>
+  );
+}
